eb4b97e25a620c5aa277c1d6973379477d29e2de
“ils15” <igorleite-ils@hotmail.com>
2020-09-29 09:13:05 -0300

    update

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1049,7 +1049,7 @@
 #if HAS_LCD_MENU
 
   // Include a page of printer information in the LCD Main Menu
-  //#define LCD_INFO_MENU
+  #define LCD_INFO_MENU
   #if ENABLED(LCD_INFO_MENU)
     //#define LCD_PRINTER_INFO_IS_BOOTSCREEN // Show bootscreen(s) instead of Printer Info pages
   #endif
