14995578c95649cc6cc741524c67193e0457dae3
“ils15” <igorleite-ils@hotmail.com>
2020-09-29 08:29:49 -0300

    fix bed

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -189,8 +189,8 @@
  * Thermal Protection parameters for the bed are just as above for hotends.
  */
 #if ENABLED(THERMAL_PROTECTION_BED)
-  #define THERMAL_PROTECTION_BED_PERIOD        20 // Seconds
-  #define THERMAL_PROTECTION_BED_HYSTERESIS     2 // Degrees Celsius
+  #define THERMAL_PROTECTION_BED_PERIOD        40 // Seconds
+  #define THERMAL_PROTECTION_BED_HYSTERESIS     4 // Degrees Celsius
 
   /**
    * As described above, except for the bed (M140/M190/M303).
