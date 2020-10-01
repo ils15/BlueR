115ac02ce56764dfce278542426637b555f48e73
“ils15” <igorleite-ils@hotmail.com>
2020-09-30 21:45:13 -0300

    update lenght

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -577,7 +577,7 @@
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
 #define PREVENT_LENGTHY_EXTRUDE
-#define EXTRUDE_MAXLENGTH 200
+#define EXTRUDE_MAXLENGTH 500
 
 //===========================================================================
 //======================== Thermal Runaway Protection =======================
