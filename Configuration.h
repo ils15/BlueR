d90dc8813bce783812e6cc366dbc3a359a2182c0
“ils15” <igorleite-ils@hotmail.com>
2020-10-28 16:49:41 -0300

    update

--- Configuration.h
+++ Configuration.h
@@ -1133,8 +1133,8 @@
 #define Y_BED_SIZE 235
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS -5
+#define X_MIN_POS -1.5
+#define Y_MIN_POS -2
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
