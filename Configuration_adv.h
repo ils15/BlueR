ab6024d0a2e83182ffcc4c748eeaebc346397a1a
“ils15” <igorleite-ils@hotmail.com>
2020-11-05 10:14:43 -0300

    update

--- Configuration_adv.h
+++ Configuration_adv.h
@@ -1083,7 +1083,7 @@
   #endif
 
   // Include a page of printer information in the LCD Main Menu
-  //#define LCD_INFO_MENU
+  #define LCD_INFO_MENU
   #if ENABLED(LCD_INFO_MENU)
     //#define LCD_PRINTER_INFO_IS_BOOTSCREEN // Show bootscreen(s) instead of Printer Info pages
   #endif
